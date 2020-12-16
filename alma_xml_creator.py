583bd7a8e2b11ad737ef8ca99493683a7e2b1998
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-12-16 11:34:36 +0100

    Append record to collection

--- alma_xml_creator.py
+++ alma_xml_creator.py
@@ -5,6 +5,11 @@ Create XML of multiple records that can be added to Alma via Import job.
 from xml.etree import ElementTree
 
 
-def create_collection_tree() -> ElementTree.Element:
+def create_collection_element() -> ElementTree.Element:
     collection = ElementTree.Element('collection')
     return collection
+
+
+def append_record(collection: ElementTree.Element):
+    record = ElementTree.SubElement(collection, 'record')
+    return record
--- test_alma_xml_creator.py
+++ test_alma_xml_creator.py
@@ -8,11 +8,21 @@ from xml.etree import ElementTree
 import alma_xml_creator
 
 
-class TestCreateCollectionTree:
+class TestCreateCollection:
 
-    collection = alma_xml_creator.create_collection_tree()
+    collection = alma_xml_creator.create_collection_element()
+    record = alma_xml_creator.append_record(collection)
 
     def test_can_add_subelement(self):
         """Output from create_collection_tree() can have SubElement"""
         testelement = ElementTree.SubElement(self.collection, "testelement")
         assert testelement.tag == "testelement"
+
+    def test_append_record(self):
+        """Appending a record works"""
+        assert self.record.tag == "record"
+
+    def test_collection_has_record(self):
+        """Is the record actually appended to the collection?"""
+        records = self.collection.findall('record')
+        assert len(records) > 0
