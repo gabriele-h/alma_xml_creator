dc596863d38dd86ecf3d7d8719d0c07cfd03ab98
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-12-17 10:36:02 +0100

    Create leader, controlfield, datafield

--- alma_xml_creator.py
+++ alma_xml_creator.py
@@ -23,6 +23,32 @@ class MarcCollection:
         def __init__(self):
             self.record = ElementTree.Element('record')
 
+        def create_subfield(self, code: str, text: str):
+            pass
+
+        def append_datafield(self, attributes: tuple, subfields: tuple):
+            datafield = ElementTree.SubElement(self.record, "datafield")
+            datafield.set("tag", attributes[0])
+            datafield.set("ind1", attributes[1])
+            datafield.set("ind2", attributes[2])
+            for subfield in subfields:
+                datafield.append(subfield)
+            return datafield
+
+        def append_controlfield(self, tag: str, text: str):
+            controlfield = self.append_field("controlfield", text)
+            controlfield.set("tag", tag)
+            return controlfield
+
+        def append_leader(self, text: str):
+            leader = self.append_field("leader", text)
+            return leader
+
+        def append_field(self, name, text):
+            field = ElementTree.SubElement(self.record, name)
+            field.text = text
+            return field
+
     @staticmethod
     def create_collection_element() -> ElementTree.Element:
         collection = ElementTree.Element('collection')
