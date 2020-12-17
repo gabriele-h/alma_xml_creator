3437d267695e20e31d23930aecd808394f155cce
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-12-17 16:38:17 +0100

    Add some docu strings for methods and functions

--- alma_xml_creator.py
+++ alma_xml_creator.py
@@ -67,6 +67,12 @@ class MarcCollection:
             self.root = Element('record')
 
         def append_datafield(self, attributes: str, subfields: str) -> SubElement:
+            """
+            For given attributes and subfields, create append a datafield to the record.
+            :param attributes: tag, ind1 and ind2 in a string without delimiters, e. g. '041  '
+            :param subfields: One string of all subfields with "$$<code>" prefix
+            :return: datafield as an xml.etree.ElementTree.SubElement
+            """
             datafield = SubElement(self.root, "datafield")
             datafield.set("tag", attributes[0:3])
             datafield.set("ind1", attributes[3])
@@ -87,21 +93,44 @@ class MarcCollection:
 
         @staticmethod
         def create_subfield(code: str, text: str) -> Element:
+            """
+            Create subfield by code and text.
+            :param code: Attribute "code" of the element with tag "subfield"
+            :param text: Content of the subfield element
+            :return: subfield as an xml.etree.ElementTree.Element
+            """
             subfield = Element("subfield")
             subfield.set("code", code)
             subfield.text = text
             return subfield
 
         def append_controlfield(self, tag: str, text: str) -> SubElement:
+            """
+            Append a controlfield to the record by attribute "tag" and content.
+            :param tag: Attribute "tag" of the element with tag "controlfield"
+            :param text: Content of the controlfield element
+            :return: controlfield as an xml.etree.ElementTree.SubElement
+            """
             controlfield = self.append_field("controlfield", text)
             controlfield.set("tag", tag)
             return controlfield
 
         def append_leader(self, text: str) -> SubElement:
+            """
+            Append a leader to the record.
+            :param text: Content of the element with tag "leader"
+            :return: leader as an xml.etree.ElementTree.SubElement
+            """
             leader = self.append_field("leader", text)
             return leader
 
         def append_field(self, name: str, text: str) -> SubElement:
+            """
+            Metafunction for appending simple fields.
+            :param name: Tag of the xml element
+            :param text: Text of the xml element
+            :return: Element as and xml.etree.ElementTree.SubElement
+            """
             field = SubElement(self.root, name)
             field.text = text
             return field
